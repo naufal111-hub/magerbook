b5270a43fcd21888f5b03921d8eff616365ee28f
sitianisa28 <sa0348185@gmail.com>
2020-01-13 22:37:48 +0700

    transaksi baruuu

--- transaksi.php
+++ transaksi.php
@@ -178,7 +178,7 @@ session_start();
                                 <div class="col-lg-9 col-12">
                                         <div class="blog-page">
                                                 <div class="page__header">
-                                                        <h2>Transaksi Peminjaman</h2>
+                                                        <h2 style="text-align: center;">Transaksi Peminjaman</h2>
                                                 </div>
                                                 <!-- Start Single Post -->
                                                 <div class="container">
@@ -239,50 +239,28 @@ session_start();
                                         
                                 </div>
                 <footer id="wn__footer" class="footer__area bg__cat--8 brown--color">
-                        <div class="footer-static-top">
-                                <div class="container">
-                                        <div class="row">
-                                                <div class="col-lg-12">
-                                                        <div class="footer__widget footer__menu">
-                                                                <div class="ft__logo">
-                                                                        <a href="index.html">
-                                                                                <img src="images/logo/3.png" alt="logo" style="height: 80px; width: 80px; border-radius: 50%;">
-                                                                        </a>
-                                                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered duskam alteration variations of passages</p>
-                                                                </div>
-                                                                <div class="footer__content">
-                                                                        <ul class="social__net social__net--2 d-flex justify-content-center">
-                                                                                <li><a href="#"><i class="bi bi-facebook"></i></a></li>
-                                                                                <li><a href="#"><i class="bi bi-google"></i></a></li>
-                                                                                <li><a href="#"><i class="bi bi-twitter"></i></a></li>
-                                                                                <li><a href="#"><i class="bi bi-linkedin"></i></a></li>
-                                                                                <li><a href="#"><i class="bi bi-youtube"></i></a></li>
-                                                                        </ul>
-                                                                </div>
-                                                        </div>
-                                                </div>
-                                        </div>
-                                </div>
+            <div class="footer-static-top">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="footer__widget footer__menu">
+                                <div class="ft__logo">
+                                    <a href="homeuser.html">
+                                        <img src="images/logo/logo.png" alt="logo" style="height: 100px; width: 100px; border-radius: 50%;">
+                                    </a>
+                                    <p>Aku Rela Di Penjara Asalkan Bersama Buku, Karena Dengan Buku Aku Bebas</p>
+                                </div> 
+                            </div>
                         </div>
-                        <div class="copyright__wrapper">
-                                <div class="container">
-                                        <div class="row">
-                                                <div class="col-lg-6 col-md-6 col-sm-12">
-                                                        <div class="copyright">
-                                                                <div class="copy__right__inner text-left">
-                                                                        <p>Copyright <i class="fa fa-copyright"></i> <a href="https://freethemescloud.com/">Free themes Cloud.</a> All Rights Reserved</p>
-                                                                </div>
-                                                        </div>
-                                                </div>
-                                                <div class="col-lg-6 col-md-6 col-sm-12">
-                                                        <div class="payment text-right">
-                                                                <img src="images/icons/payment.png" alt="logo"/> 
-                                                        </div>
-                                                </div>
-                                        </div>
-                                </div>
-                        </div>
-                </footer>
+
+                    </div>
+                </div>
+            </div>
+             
+                    </div>
+                </div>
+            </div>
+        </footer>
                 <!-- //Footer Area -->
         </div>
         <!-- //Main wrapper -->
